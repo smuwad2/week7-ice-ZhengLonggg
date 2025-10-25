906c24acb98a35073fedfdce91099ee6cb197632
Liew Zheng Long <166126524+ZhengLonggg@users.noreply.github.com>
2025-10-25 17:18:41 +0000

    first three trees

--- src/components/Ex2.vue
+++ src/components/Ex2.vue
@@ -3,9 +3,22 @@
         // Add code in appropriate places
         data() {
             return {
-                items: ['keyboard', 'mouse', 'iPhone', 'macbook', 'adapter']
+                items: ['keyboard', 'mouse', 'iPhone', 'macbook', 'adapter'],
+                newItem: ''
             }
             
+        },
+        methods:{
+            deleteItem(index){
+                this.items.splice(index,1);
+            },
+            addItem(){
+                if(this.newItem.trim()!==''){
+                    this.items.push(this.newItem.trim());
+                    this.newItem = '';
+                }
+            }
+
         }
     }
 </script>
@@ -14,5 +27,13 @@
 
     <h2>Shopping Cart</h2>
     <!-- TODO: Add Code Here -->
+    <ul>
+    <li v-for="(item, index) in items" :key="index">
+        {{item}}<button v-on:click="deleteItem(index)">Delete!</button>
+    </li>
+    </ul>
+    <input type="text" v-model="newItem">
+    <button v-on:click="addItem">Add!</button>
    
 </template>
+
